1200642194173f6d1ff724baeeed4e1d29c37fe0
adrian fahrezi <10mm2.adrianfahrezi@gmail.com>
2026-02-06 17:38:42 +0700

    Optimize domain checking in checker_success.sh
    Refactor script to use a single loop for checking domains.

--- 04-Automation-Scripting/scripts/checker_success.sh
+++ 04-Automation-Scripting/scripts/checker_success.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+for domain in $(cat subdomains.txt); do
 
-for ping in $(cat subdomains.txt); do
-
+    # Gunakan variabel $domain langsung. Jangan pakai 127.0.0
     if ping -c 1 -W 1 $domain > /dev/null; then
         echo "[+] Target $domain ditemukan! (UP)"
     else
